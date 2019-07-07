fd9264fb2395324bd9142de530411c22731d7ecb
mtuda <m.mtudarona@gmail.com>
2019-07-07 15:53:21 +0900

    GoForwardUnityChan DebugLog接触判定
    それぞれのブロックで接触判定できているところまでです

---
+++ Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    //キューブの移動速度
+    private float speed = -0.2f;
+
+    //消滅位置
+    private float deadLine = -10;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        //画面外に出たら破棄する
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    //ブロックが衝突した場合に呼ばれる関数
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("接触しました");
+        GetComponent<AudioSource>().volume = 1;
+    }
+}
